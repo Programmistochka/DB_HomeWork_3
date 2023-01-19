c301c1e136492c713d6a502afe5d93bd40185a3c
Programmistochka <programmistochka@list.ru>
2023-01-20 00:48:26 +1200

    Initial commit

---
+++ Task_2_SQL_SELECT.sql
@@ -0,0 +1,31 @@
+--SELECT запросы
+-- Название и год выхода альбомов, вышедших в 2018 г
+SELECT name, album_year 
+FROM albums
+where album_year = 2018;
+
+--Название и продолжительность самого длинного трека
+SELECT name, duration 
+FROM tracks
+ORDER BY duration DESC
+LIMIT 1;
+
+--Названия треков, продолжительность которых не менее 3,5 минуты
+SELECT name 
+FROM tracks
+WHERE duration > '3:30';
+
+--Названия сборников, вышедших с 2018 по 2020 гг
+SELECT name, collect_year 
+FROM collections
+WHERE collect_year BETWEEN 2018 and 2020;
+
+--Исполнители, чье имя состоит из одного слова
+SELECT name 
+FROM performers
+WHERE name NOT LIKE '% %';
+
+--Название треков, которые содержат слово "мой"/"my"
+SELECT name 
+FROM tracks
+WHERE LOWER(name) LIKE '%my%' OR LOWER(name) LIKE '%мой%';
